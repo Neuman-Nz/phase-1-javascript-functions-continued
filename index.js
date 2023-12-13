f2da07dd7c60ae6fd512d6d994a32cde6d763c35
neuman <owiroadams@gmail.com>
2023-12-13 12:46:34 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(x = "roller-skate"){
+    let message =`This Saturday, I want to ${x}!`
+    return message;
+}
+
+function mondayWork(y = "go to the office" , z = "work from home"){
+    let message = `This Monday, I will ${y}.`
+    let message2 = `This Monday, I will ${z}.`
+   
+    return message2 && message;
+}
+
+function wrapAdjective(a) {
+    const innerFunction = function (b) {
+      return `You are ${a}${b}${a}!`;
+    };
+    return innerFunction
+  }
+  
+  wrapAdjective("*")("a hard worker");
